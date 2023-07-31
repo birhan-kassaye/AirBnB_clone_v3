3cadbe8652cf1aefd4aade1aec55d67466b6249c
MelakuBi <MelakuBirhanu@gmail.com>
2023-07-31 12:38:52 -0700

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import app_views from api.v1.views
-from flask import jsonify
-=======
 #!/usr/bin/python3
 """ index """
 from api.v1.views import app_views
@@ -13,19 +9,15 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 from models.user import User
->>>>>>> master
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ returns a JSON """
     return jsonify({"status": "OK"})
 
-
-<<<<<<< HEAD
-@app_views.route("/stats")
-=======
 @app_views.route("/stats", strict_slashes=False)
->>>>>>> master
+
 def stats():
     clases = {"amenities": Amenity, "cities": City, "places": Place,
               "reviews": Review, "states": State, "users": User}
